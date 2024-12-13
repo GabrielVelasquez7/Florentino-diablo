0eb61ddbb817031ae4bf09e008a3e851e1e7a56b
GabrielVelasquez7 <74571562+GabrielVelasquez7@users.noreply.github.com>
2024-12-13 12:46:00 -0400

    Ajustes

--- Keyobject.gd
+++ Keyobject.gd
@@ -16,27 +16,28 @@ func _process(delta):
 			queue_free()
 
 
-func spawn(key:int, pos:Vector2)->void:
+func spawn(key:int, pos:Vector2) -> void:
 	position = pos
 	match key:
 		0:
-			selected_key = KEY_LEFT
+			selected_key = "left"  # Usar "ui_left" para la tecla izquierda
 			rotation_degrees = 0
 		1:
-			selected_key = KEY_RIGHT
+			selected_key = KEY_RIGHT  # Usar "ui_right" para la tecla derecha
 			rotation_degrees = 180
 		2:
-			selected_key = KEY_UP
+			selected_key = KEY_UP  # Usar "ui_up" para la tecla arriba
 			rotation_degrees = 90
 		3:
-			selected_key = KEY_DOWN
+			selected_key = KEY_DOWN # Usar "ui_down" para la tecla abajo
 			rotation_degrees = -90
+
 
 
 
 func _on_area_entered(area):
 	inside_area = true
-
+	print("debug=> que bien!")
 
 func _on_area_exited(area):
 	inside_area = false
